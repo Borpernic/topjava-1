f606d5520a80c2dc65fb9f1d48cdd0b831e1c031
borpernic <borpernic@gmail.com>
2018-04-26 14:16:22 +0300

    add populate user_meals table

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,12 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO user_meals (user_id, dateTime,description,calories) VALUES
+  (100000,'2017-04-26 10:00:00','Затрак',500),
+  (100000,'2017-04-26 14:00:00','Обед',1000),
+  (100000,'2017-04-26 18:00:00','Ужин',500),
+  (100001,'2017-04-26 10:00:00','Затрак',500),
+  (100001,'2017-04-26 14:00:00','Обед',1000),
+  (100001,'2017-04-26 18:00:00','Ужин',500);
+
