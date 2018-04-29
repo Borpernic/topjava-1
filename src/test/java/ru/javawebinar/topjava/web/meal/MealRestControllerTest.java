f3ebcce2d387a8074a663ef76ca20ae7dd2e4a7a
bn <borpernic@gmail.com>
2018-04-30 01:42:39 +0300

    meal test added

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,36 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MealRestControllerTest {
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void getBetween() {
+    }
+
+    @Test
+    public void getBetween1() {
+    }
+}
