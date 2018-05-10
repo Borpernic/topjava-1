2b329e2272970b94c8fa1a2cafa96aa52e0e415c
borpernic <borpernic@gmail.com>
2018-05-10 18:05:32 +0300

    Start implementations JpaMealRepository

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,21 +1,41 @@
 package ru.javawebinar.topjava.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id"),
+        @NamedQuery(name = Meal.DELETE_ALL, query = "DELETE FROM Meal m WHERE m.id=:id and m.userId=:userId"),
+        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m RIGHT JOIN FETCH m.user.id ORDER BY m.dateTime desc ")
+})
 @Entity
 @Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = {"id"}, name = "meals_pkey"),
         @UniqueConstraint(columnNames = {"user_id"}, name = "meals_user_id_fkey")})
 
 public class Meal extends AbstractBaseEntity {
+
+    public static final String DELETE = "User.delete";
+    public static final String DELETE_ALL = "Meal.deleteAll";
+    public static final String ALL_SORTED = "Meal.getAllSorted";
+
+    @Column(name = "date_time", nullable = false)
+    @NotNull
     private LocalDateTime dateTime;
 
+
+    @Column(name = "description", nullable = false)
+    @NotNull
     private String description;
 
+    @Column(name = "calories", nullable = false)
+    @NotNull
     private int calories;
 
+    @Column(name = "user_id", nullable = false)
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
